ee8fdda93662c79da2e9436ddc75c01cfd226756
Omar Islam Laskar <omarislamlaskar@gmail.com>
2026-04-13 14:26:17 +0800

    verif: add RISC-V ISA test suite and automated runner script

---
+++ tests/isa/env/encoding.h
@@ -0,0 +1,22 @@
+#ifndef RISCV_ENCODING_H
+#define RISCV_ENCODING_H
+
+#define MSTATUS_MPP         0x00001800
+#define MSTATUS_FS          0x00006000
+
+#define CAUSE_MISALIGNED_FETCH    0x0
+#define CAUSE_FETCH_ACCESS        0x1
+#define CAUSE_ILLEGAL_INSTRUCTION 0x2
+#define CAUSE_BREAKPOINT          0x3
+#define CAUSE_MISALIGNED_LOAD     0x4
+#define CAUSE_LOAD_ACCESS         0x5
+#define CAUSE_MISALIGNED_STORE    0x6
+#define CAUSE_STORE_ACCESS        0x7
+#define CAUSE_USER_ECALL          0x8
+#define CAUSE_SUPERVISOR_ECALL    0x9
+#define CAUSE_MACHINE_ECALL       0xb
+#define CAUSE_FETCH_PAGE_FAULT    0xc
+#define CAUSE_LOAD_PAGE_FAULT     0xd
+#define CAUSE_STORE_PAGE_FAULT    0xf
+
+#endif
